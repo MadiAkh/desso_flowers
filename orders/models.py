b1f6325521f0336b6dce77943c0e1f2fe1f5b59e
Madi <madiq1994@mail.ru>
2026-03-28 01:49:08 +0500

    fix: resolve OrderItem conflicts and migrations after merge

--- orders/models.py
+++ orders/models.py
@@ -113,22 +113,6 @@ class OrderItem(models.Model):
     def total_price(self):
         return self.price * self.quantity
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(
-        Order,
-        related_name='items',
-        on_delete=models.CASCADE
-    )
-
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField()
-
-    @property
-    def total_price(self):
-        return self.price * self.quantity
-
 
 class Wishlist(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wishlist")
