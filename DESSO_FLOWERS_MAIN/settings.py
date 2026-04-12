1b73400c434bc6b7ab662c741dbd98cb8555dd44
Madi <madiq1994@mail.ru>
2026-04-12 22:48:27 +0500

    fixes for mobile view

--- DESSO_FLOWERS_MAIN/settings.py
+++ DESSO_FLOWERS_MAIN/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u(#y1%x(l3(ure5k=h@^isyyo_zxh5finl*)bm*e70*t2t5jm)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.106", 'localhost', '127.0.0.1']
 
 
 AUTH_USER_MODEL = 'personal_account.DessoUser'
